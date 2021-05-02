bafea5be0094b92fe6211d4ad582316597f1a49e
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2021-05-02 20:51:56 +0700

    fix bug

--- resources/views/livewire/admin-show.blade.php
+++ resources/views/livewire/admin-show.blade.php
@@ -6,13 +6,13 @@
     }
 @endphp
 <div>
-    <div class="flex justify-end">
-        <button class="bg-green-500 text-white px-4 py-1 rounded-md focus:outline-none mb-2 flex" @click="open = true">
-            <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
-            Tambah Admin
-        </button>
-    </div>
     <div x-data="{ open: false }">
+        <div class="flex justify-end">
+            <button class="bg-green-500 text-white px-4 py-1 rounded-md focus:outline-none mb-2 flex" @click="open = true">
+                <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                Tambah Admin
+            </button>
+        </div>
         <form wire:submit.prevent="store">
             <div x-show="open" class="fixed inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" style="z-index: 9999">
                 <div class="flex items-center justify-center min-h-screen text-center sm:block sm:p-0">
@@ -130,45 +130,43 @@
                                 <div class="flex justify-center rounded-lg text-lg" role="group">
                                     <div class="flex justify-center rounded-lg text-lg" role="group">
                                         <livewire:admin-change :idadmin="$a->id" :role="$role" :agency="$agency" :key="$key">
-                                        <div x-data="{ open: false }">
-                                            <button @click="open = true" class="bg-white text-gray-600 hover:bg-red-200 hover:text-red-600 border border-l-0 border-gray-300 rounded-r-md px-4 py-2 mx-0 outline-none">
+                                        <div x-data="{ isOpen: false }">
+                                            <button @click="isOpen = true" class="bg-white text-gray-600 hover:bg-red-200 hover:text-red-600 border border-l-0 border-gray-300 rounded-r-md px-4 py-2 mx-0 outline-none">
                                                 <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>    
                                             </button>
-                                            <div x-data="{ open: false }">
-                                                <div x-show="open"  class="fixed inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" style="z-index: 9999999999">
-                                                    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                                                        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-                                                        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-                                                        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                                                            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                                                <div class="sm:flex sm:items-start">
-                                                                    <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                                                        <!-- Heroicon name: outline/exclamation -->
-                                                                        <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                                                                        </svg>
-                                                                    </div>
-        
-                                                                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                                                        <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                                                                            Hapus Admin
-                                                                        </h3>
-                                                                        <div class="mt-2">
-                                                                            <p class="text-sm text-gray-500">
-                                                                                Apakah kamu yakin ingin menghapus admin <strong>{{ $a->fullname }}</strong>? </br>Data yang sudah dihapus tidak akan bisa dikembalikan kembali.
-                                                                            </p>
-                                                                        </div>
+                                            <div x-show="isOpen"  class="fixed inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" style="z-index: 9999999999">
+                                                <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                                                    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+                                                    <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                                                    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                                                        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                                            <div class="sm:flex sm:items-start">
+                                                                <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                                    <!-- Heroicon name: outline/exclamation -->
+                                                                    <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                                                                    </svg>
+                                                                </div>
+    
+                                                                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                                                    <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                                                                        Hapus Admin
+                                                                    </h3>
+                                                                    <div class="mt-2">
+                                                                        <p class="text-sm text-gray-500">
+                                                                            Apakah kamu yakin ingin menghapus admin <strong>{{ $a->fullname }}</strong>? </br>Data yang sudah dihapus tidak akan bisa dikembalikan kembali.
+                                                                        </p>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                                                <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" onclick="location.href='/admin/{{ $a->id }}/delete'">
-                                                                    Saya Yakin
-                                                                </button>
-                                                                <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                                                                    Batal
-                                                                </button>
-                                                            </div>
+                                                        </div>
+                                                        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                                            <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" onclick="location.href='/admin/{{ $a->id }}/delete'">
+                                                                Saya Yakin
+                                                            </button>
+                                                            <button @click="isOpen = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                                                                Batal
+                                                            </button>
                                                         </div>
                                                     </div>
                                                 </div>
